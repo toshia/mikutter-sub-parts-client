96c5b1adcfd25e5fe02291d156db595cdc6230e7
Toshiaki Asai <toshi.alternative@gmail.com>
2013-04-21 14:10:35 +0900

    みっくストアで正しく扱えるように変更を加えた

---
+++ spec
@@ -0,0 +1,11 @@
+---
+slug: :sub_parts_client
+depends:
+  mikutter: 0.2.0.1052
+  plugin:
+  - gtk
+version: '1.0'
+author: toshi_a
+name: subparts client
+description: 各ツイートを投稿するために使われたTwitterクライアントを表示します
+repository: git://github.com/toshia/mikutter-sub-parts-client.git
--- sub_parts_client.rb
+++ sub_parts_client.rb
@@ -4,45 +4,47 @@ miquire :mui, 'sub_parts_helper'
 require 'gtk2'
 require 'cairo'
 
-# ツイートが投稿されるのに使われたクライアントアプリケーションの名前をTL上に表示する
-class Gdk::SubPartsClient < Gdk::SubParts
-  regist
+Plugin.create :sub_parts_client do
+  # ツイートが投稿されるのに使われたクライアントアプリケーションの名前をTL上に表示する
+  class Gdk::SubPartsClient < Gdk::SubParts
+    regist
 
-  def initialize(*args)
-    super
-    @margin = 2
-    if message and not helper.visible?
-      sid = helper.ssc(:expose_event, helper){
-        helper.on_modify
-        helper.signal_handler_disconnect(sid)
-        false } end
+    def initialize(*args)
+      super
+      @margin = 2
+      if message and not helper.visible?
+        sid = helper.ssc(:expose_event, helper){
+          helper.on_modify
+          helper.signal_handler_disconnect(sid)
+          false } end
+    end
+
+    def render(context)
+      if helper.visible? and message
+        context.save{
+          layout = main_message(context)
+          context.translate(width - (layout.size[0] / Pango::SCALE) - @margin, 0)
+          context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
+          context.show_pango_layout(layout) } end end
+
+    def height
+      main_message.size[1] / Pango::SCALE end
+    memoize :height
+
+    private
+
+    def main_message(context = dummy_context)
+      layout = context.create_pango_layout
+      layout.font_description = Pango::FontDescription.new(UserConfig[:mumble_basic_font])
+      layout.alignment = Pango::ALIGN_RIGHT
+      if(message[:source])
+        layout.text = 'via '+message[:source].gsub(/<.*?>/, '')
+      else
+        layout.text = '' end
+      layout end
+
+    def message
+      helper.message end
+
   end
-
-  def render(context)
-    if helper.visible? and message
-      context.save{
-        layout = main_message(context)
-        context.translate(width - (layout.size[0] / Pango::SCALE) - @margin, 0)
-        context.set_source_rgb(*(UserConfig[:mumble_basic_color] || [0,0,0]).map{ |c| c.to_f / 65536 })
-        context.show_pango_layout(layout) } end end
-
-  def height
-    main_message.size[1] / Pango::SCALE end
-  memoize :height
-
-  private
-
-  def main_message(context = dummy_context)
-    layout = context.create_pango_layout
-    layout.font_description = Pango::FontDescription.new(UserConfig[:mumble_basic_font])
-    layout.alignment = Pango::ALIGN_RIGHT
-    if(message[:source])
-      layout.text = 'via '+message[:source].gsub(/<.*?>/, '')
-    else
-      layout.text = '' end
-    layout end
-
-  def message
-    helper.message end
-
 end
